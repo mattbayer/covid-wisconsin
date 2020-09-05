22e25d660f85161ffcd2161d58870e00b2b72cdc
mattbayer <67168398+mattbayer@users.noreply.github.com>
2020-09-05 07:51:58 -0500

    Plots for infection estimates

--- WorkCorona.py
+++ WorkCorona.py
@@ -68,14 +68,16 @@ gudata = gudata.set_index('Date')
 est['Gu Estimate'] = gudata['true_inf_est_7day_ma']
 est['Bayer Estimate'] = est['Estimated infection prevalence'] * popdata['WI'] / 7
 est['Detected x10'] = est['cases'] * 10
+est['Detected Cases'] = est['cases']
 
 # back-dated deaths, assume IFR 1%
 deaths = select['DTH_NEW'].rolling(window=7, center=False).mean()
 deaths.index = deaths.index - datetime.timedelta(days=14)
-est['Deaths (IFR 0.75%)'] = deaths * 150
+est['Deaths (IFR 0.75%, 14 day shift)'] = deaths * 150
 
-est.plot(title='Wisconsin New Infection Estimates', y=['Detected x10', 'Gu Estimate', 'Bayer Estimate', 'Deaths (IFR 0.75%)'])
+est.plot(title='Wisconsin New Infection Estimates', y=['Detected Cases', 'Gu Estimate', 'Bayer Estimate', 'Deaths (IFR 0.75%, 14 day shift)'])
 
+est.plot(title='Wisconsin New Infection Estimates', y=['Detected Cases', 'Gu Estimate', 'Bayer Estimate'])
 
 
 #%% Work on hospitalization status
