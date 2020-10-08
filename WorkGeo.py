b19cf9519f8843d31fb43d326644bb93578e982b
Bayer, Matthew A <matthew.bayer@ge.com>
2020-10-08 15:00:46 -0500

    Geo work and data update

--- WorkGeo.py
+++ WorkGeo.py
@@ -45,8 +45,6 @@ countiesUSA = gpd.read_file(shapefile)
 # filter on wisconsin
 countiesWI = countiesUSA[countiesUSA.STATEFP == '55']
 
-
-#%% 
 # reindex on county name
 countiesWI = countiesWI.set_index('NAME')
 # sort by name
@@ -54,8 +52,8 @@ countiesWI = countiesWI.sort_index()
 # add Population column
 countiesWI['Population'] = popdata
 
-# chloropleth by population
-base = countiesWI.plot(column='Population', edgecolor='w', linewidth=0.5)
+# # chloropleth by population from geopandas
+# base = countiesWI.plot(column='Population', edgecolor='w', linewidth=0.5)
 
 
 #%%
@@ -99,85 +97,114 @@ countiesWI['Hospitalizations per 100,000'] = countiesWI['Hospitalizations'] / co
 import json
 import plotly.express as px
 from plotly.offline import plot as pplot
+import plotly.graph_objects as go
+
+do_choropleth = False
 
 plotcol = 'Cases per 100,000'
 plotcol2 = 'Hospitalizations per 100,000'
 hosp_scale = [0, 4]
 
-# with urllib.request.urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    
 # filter counties shapefile to WI, convert to JSON format string, then decode 
 # to dictionary with json.loads()
-# countiesJS = json.loads(countiesUSA[countiesUSA.STATEFP == '55'].to_json())
 countiesJS = json.loads(countiesWI.to_json())
 
-fig = px.choropleth(countiesWI, 
-                    geojson=countiesJS, 
-                    locations=countiesWI.index, 
-                    color=plotcol, 
-                    color_continuous_scale=px.colors.sequential.Blues,
-                    title='Cases by County',
-                    projection='mercator')
-fig.update_geos(fitbounds='locations', visible=False)
+#%% Choropleth maps
+if do_choropleth:
+    fig = px.choropleth(countiesWI, 
+                        geojson=countiesJS, 
+                        locations=countiesWI.index, 
+                        color=plotcol, 
+                        color_continuous_scale=px.colors.sequential.Blues,
+                        title='Cases by County',
+                        projection='mercator')
+    fig.update_geos(fitbounds='locations', visible=False)
+    
+    pplot(fig, filename='.\\plots\\plotly\\temp.html' )
+    
+    fig2 = px.choropleth(countiesWI, 
+                        geojson=countiesJS, 
+                        locations=countiesWI.index, 
+                        color=plotcol2, 
+                        color_continuous_scale=px.colors.sequential.Oranges,
+                        range_color=hosp_scale,
+                        title='Hospitalizations by County',
+                        projection='mercator')
+    fig2.update_geos(fitbounds='locations', visible=False)
+    
+    
+    pplot(fig2, filename='.\\plots\\plotly\\temp2.html' )
 
-pplot(fig, filename='.\\plots\\plotly\\temp.html' )
 
-fig2 = px.choropleth(countiesWI, 
-                    geojson=countiesJS, 
-                    locations=countiesWI.index, 
-                    color=plotcol2, 
-                    color_continuous_scale=px.colors.sequential.Oranges,
-                    range_color=hosp_scale,
-                    title='Hospitalizations by County',
-                    projection='mercator')
-fig2.update_geos(fitbounds='locations', visible=False)
-
-
-pplot(fig2, filename='.\\plots\\plotly\\temp2.html' )
-
-
-#%% try a different map 
-import plotly.graph_objects as go
+#%% Bubble map - size is numbers, color is per-population
 
 # get latitude and longitude of centroids of counties for plotting
 # this will give warning but I don't care
 countiesWI['plotlon'] = countiesWI.geometry.centroid.x
 countiesWI['plotlat'] = countiesWI.geometry.centroid.y
-# move Milwaukee's plot center to the right a bit to make more room
+# move Milwaukee's plot center to the right a bit to make more room for its bubble
 countiesWI.loc['Milwaukee', 'plotlon'] = countiesWI.loc['Milwaukee', 'plotlon'] + 0.15
+
+# append 'County' for display names
+display_names = [n + ' County' for n in countiesWI.index]
+
 # set scales for sizes of bubbles
 popscale = 300
-casescale = 0.3
-hospscale = 0.006
+casescale = 0.25
+hospscale = casescale*.05;   # so that bubbles are same size if hosp = 5% of cases
 
-# Cases
+#%% Cases
 plotcol = 'Cases per 100,000'
+# line_colors = {'land':'darkgrey', 'border':'lightgray', 'marker':'dimgray'}
+# line_colors = {'land':'darkgrey', 'border':'dimgray', 'marker':'lightgray'}
+# line_colors = {'land':'lightgray', 'border':'white', 'marker':'dimgray'}
+line_colors = {'land':'lightgray', 'border':'darkgray', 'marker':'dimgray'}
+
+# background map of counties
 fig = px.choropleth(countiesWI, 
                     geojson=countiesJS, 
                     locations=countiesWI.index, 
-                    color_discrete_sequence=['white'],
+                    color_discrete_sequence=[line_colors['land']],
                     title='Cases by County',
                     projection='mercator')
 
-# fig = go.Figure()
+# turn off hover tooltips for this layer - have to set both of these because
+# hovertemplate is set automatically and it supersedes hoverinfo.
+# Also take out legend because it's not very useful right now; I could add
+# a fancier custom legend later.
+fig.update_traces(hovertemplate=None, 
+                  hoverinfo='skip', 
+                  marker_line_color=line_colors['border'],
+                  showlegend=False)
+
 
 fig.add_trace(go.Scattergeo(lon=countiesWI.plotlon,
                             lat=countiesWI.plotlat,
-                            marker=dict(size=countiesWI.Cases / casescale, #size=countiesWI.Population / popscale,
+                            text=display_names,
+                            customdata=countiesWI.Population,
+                            marker=dict(size=countiesWI.Cases, #size=countiesWI.Population / popscale,
+                                        sizeref=casescale,
                                         color=countiesWI[plotcol],
                                         sizemode='area',
                                         colorscale='Blues'),
-                            line=dict(color='lightgray')
+                            line=dict(color=line_colors['marker']),
+                            hovertemplate=
+                            '<b>%{text}</b><br>' +
+                            'Population: %{customdata:.0f}<br>' +
+                            'Cases: %{marker.size:.1f}<br>' + 
+                            'Cases per 100K: %{marker.color:.1f}'
                             ))
 
+# only display wisconsin counties, not whole world
 fig.update_geos(fitbounds='locations', visible=False)
 
 pplot(fig, filename='.\\plots\\plotly\\temp.html')
 
 
-# Hospitalizations
+#%% Hospitalizations
 plotcol = 'Hospitalizations per 100,000'
 
+# background map of counties
 fig = px.choropleth(countiesWI, 
                     geojson=countiesJS, 
                     locations=countiesWI.index, 
@@ -185,17 +212,32 @@ fig = px.choropleth(countiesWI,
                     title='Hospitalizations by County',
                     projection='mercator')
 
-# fig = go.Figure()
+# turn off hover tooltips for this layer - have to set both of these because
+# hovertemplate is set automatically and it supersedes hoverinfo
+# Also take out legend because it's not very useful right now; I could add
+# a fancier custom legend later.
+fig.update_traces(hovertemplate=None, 
+                  hoverinfo='skip',
+                  showlegend=False)
+
 
 fig.add_trace(go.Scattergeo(lon=countiesWI.plotlon,
                             lat=countiesWI.plotlat,
-                            marker=dict(size=countiesWI.Hospitalizations / hospscale, #size=countiesWI.Population / popscale,
+                            text=display_names,
+                            customdata=countiesWI.Population,
+                            marker=dict(size=countiesWI.Hospitalizations,
+                                        sizeref=hospscale,
                                         color=countiesWI[plotcol],
                                         cmin=hosp_scale[0],
                                         cmax=hosp_scale[1],
                                         sizemode='area',
                                         colorscale='Oranges'),
-                            line=dict(color='lightgray')
+                            line=dict(color='lightgray'),
+                            hovertemplate=
+                            '<b>%{text}</b><br>' +
+                            'Population: %{customdata:.0f}<br>' +
+                            'Hospitalizations: %{marker.size:.1f}<br>' + 
+                            'Hosp per 100K: %{marker.color:.1f}'
                             ))
 
 fig.update_geos(fitbounds='locations', visible=False)
