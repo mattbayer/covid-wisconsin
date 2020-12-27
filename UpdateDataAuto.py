e6822b915abc2df23ba808ee30a827c6abb7d060
mattbayer <67168398+mattbayer@users.noreply.github.com>
2020-12-27 13:27:10 -0600

    Update UpdateDataAuto.py
    Add state line plots

--- UpdateDataAuto.py
+++ UpdateDataAuto.py
@@ -9,5 +9,34 @@ Created on Thu Dec 24 12:33:03 2020
 
 import covid
 
+#%% Update data
 covid.update_covid_data_wi('state')
 covid.update_covid_data_wi('county')
+
+state = covid.read_covid_data_wi('state')
+
+
+#%% Dashboard - state line plots
+plotpath = '.\\docs\\assets\\plotly'
+
+# reduce and rename at state level
+col_rename = {'POS_NEW': 'Cases', 'TEST_NEW': 'Tests', 'DTH_NEW': 'Deaths', 'HOSP_NEW': 'Hospitalizations'}
+state = state.rename(columns=col_rename)
+
+# Cases / Tests line plot
+covid.plotly_casetest(sourcedata=state, 
+                      case_col='Cases', 
+                      test_col='Tests', 
+                      date_col='Date', 
+                      savefile=plotpath + '\\Cases-Tests-WI.html',
+                      showfig=False,
+                      )
+
+# Deaths / Hospitalizations line plot
+covid.plotly_deadhosp(sourcedata=state, 
+                      hosp_col='Hospitalizations', 
+                      dead_col='Deaths', 
+                      date_col='Date', 
+                      savefile=plotpath + '\\Deaths-Hosp-WI.html',
+                      showfig=False,
+                      )
