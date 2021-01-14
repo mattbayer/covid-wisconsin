4c63f81741cc45f2467253c0c0108a73e60be8a9
mattbayer <matthew.bayer@ge.com>
2021-01-14 10:06:30 -0600

    Update BlogPrisons.py
    Work on fill-bubble plot

--- BlogPrisons.py
+++ BlogPrisons.py
@@ -128,33 +128,37 @@ def plotly_colorbubble2(
     # then overwrite
     plotlabels = plotlabels_default
     
-    # Plotly needs a JSON format string for plotting arbitrary shapes, so -
-    # convert geodata to JSON format string, then decode to dictionary with json.loads()
-    geoJS = json.loads(geo_background.to_json())
+    # Plot background map
+    fig = plotly_backmap(geo_background)
+    # fig = go.Figure()
+    
+    # # Plotly needs a JSON format string for plotting arbitrary shapes, so -
+    # # convert geodata to JSON format string, then decode to dictionary with json.loads()
+    # geoJS = json.loads(geo_background.to_json())
 
     # Colors for the background map
     line_colors = {'land':'lightgray', 'border':'darkgray', 'marker':'dimgray'}
     
-    # Background map
-    fig = px.choropleth(
-        geo_background, 
-        geojson=geoJS,
-        locations=geo_background.index,
-        color_discrete_sequence=[line_colors['land']],
+    # # Background map
+    # fig = px.choropleth(
+    #     geo_background, 
+    #     geojson=geoJS,
+    #     locations=geo_background.index,
+    #     color_discrete_sequence=[line_colors['land']],
 
-        projection='mercator',
-        )
+    #     projection='mercator',
+    #     )
     
-    # turn off hover tooltips for this layer - have to set both of these because
-    # hovertemplate is set automatically and it supersedes hoverinfo.
-    # Also take out legend because it's not very useful right now; I could add
-    # a fancier custom legend later.
-    fig.update_traces(
-        hovertemplate=None, 
-        hoverinfo='skip', 
-        marker_line_color=line_colors['border'],
-        showlegend=False,
-        )
+    # # turn off hover tooltips for this layer - have to set both of these because
+    # # hovertemplate is set automatically and it supersedes hoverinfo.
+    # # Also take out legend because it's not very useful right now; I could add
+    # # a fancier custom legend later.
+    # fig.update_traces(
+    #     hovertemplate=None, 
+    #     hoverinfo='skip', 
+    #     marker_line_color=line_colors['border'],
+    #     showlegend=False,
+    #     )
 
     fig.update_layout(title=plotlabels['title'])
     
@@ -282,7 +286,7 @@ def plotly_colorbubble2(
     fig.update_layout(margin=dict(l=30,b=20))
     
     # Only display this specific geography, not whole world
-    fig.update_geos(fitbounds='locations', visible=False)
+    # fig.update_geos(fitbounds='locations', visible=False)
     
     # plot and save as html, with plotly JS library loaded from CDN
     fig.write_html(
@@ -298,6 +302,199 @@ def plotly_colorbubble2(
     return fig
 
 
+def plotly_fillbubble(
+        geo_background,
+        geodata,
+        outercol,
+        innercol,
+        latcol,
+        loncol,
+        popcol='Population',
+        size_factor=1,
+        color_range=[0, 600],
+        colorscale=None,
+        location_names=None,
+        plotlabels=None,
+        savefile='.\\temp.html',
+        fig_height='100%',
+        showfig = True,
+        ):
+    """Create interactive plotly map figure, with concentric bubbles.
+    The outer bubble is a max value, and the inner fills it up so this kind of
+    map can show a value and a percentage at the same time.
+    
+    geodata     -- GeoPandas DataFrame 
+    outercol    -- Column to use for outer bubble size
+    innercol    -- Column to use for inner bubble size, should be < outer.
+    fig_height  -- html tag for height of the figure. Default is to fill the div; could also specify pixels.
+    showfig     -- flag for displaying the figure after it is created
+    """
+    # Input process plotlabels
+    plotlabels_default = dict(title='Fill-Bubble Map',
+                              outerlabel=outercol,
+                              innerlabel=innercol,
+                              outerlegend=outercol,
+                              innerlegend=innercol,
+                              )
+    if plotlabels is not None:
+        # replace default values with the passed values
+        for n in plotlabels.keys():
+            plotlabels_default[n] = plotlabels[n]
+    # then overwrite
+    plotlabels = plotlabels_default
+    
+    # Plot background map
+    fig = plotly_backmap(geo_background)
+    
+    # Colors for the background map
+    line_colors = {'land':'lightgray', 'border':'darkgray', 'marker':'dimgray',
+                   'outer':'white', 'inner':'orange'}
+    
+    # Add title
+    fig.update_layout(title=plotlabels['title'])   
+    
+    # Create display names for tooltip
+    if location_names is None:
+        location_names = geodata.index
+    
+    # Create the outer bubble figure
+    fig.add_trace(
+        go.Scattergeo(
+            lon=geodata[loncol],
+            lat=geodata[latcol],
+            text=location_names,
+            customdata=geodata[innercol],
+            marker=dict(
+                size=geodata[outercol], 
+                sizeref=size_factor,
+                sizemode='area',
+                color=line_colors['outer'],
+                # opacity=1,
+                ),
+            line=dict(color=line_colors['marker']),
+            hovertemplate=
+                '<b>%{text}</b><br>' +
+                plotlabels['outerlabel']  + ' : %{marker.size:.0f}<br>' + 
+                plotlabels['innerlabel'] + ' : %{customdata:.0f}'+
+                '<extra></extra>',
+            name=outercol,
+            showlegend=False,
+            legendgroup=outercol,
+            )
+        )
+    
+    # Create the inner bubble figure
+    fig.add_trace(
+        go.Scattergeo(
+            lon=geodata[loncol],
+            lat=geodata[latcol],
+            text=location_names,
+            customdata=geodata[popcol],
+            marker=dict(
+                size=geodata[innercol], 
+                sizeref=size_factor,
+                sizemode='area',
+                color=line_colors['inner'],
+                # opacity=1,
+                ),
+            # line=dict(color=line_colors['marker']),
+            # No hover info, it was included in the outer bubble
+            hovertemplate=None, 
+            hoverinfo='skip', 
+            name=innercol,
+            showlegend=False,
+            legendgroup=innercol,
+            )
+        )
+    
+    # Add artificial traces to create a legend
+    # Max size in pixels from web search and experiment; 
+    # then convert to sizes in the units of the sizecol
+    legend_marker_pixel_max = 16    # diameter
+    legend_marker_outercol_max = (legend_marker_pixel_max)**2 * size_factor / 2   
+    sizes_outercol = np.array([1, 0.25]) * legend_marker_outercol_max
+    # round to nearest first significant digit
+    power10 = 10**np.floor(np.log10(sizes_outercol))
+    sizes_outercol = np.round(sizes_outercol / power10) * power10
+    # round up to whole integer in case lowest is < 1
+    sizes_outercol = np.ceil(sizes_outercol)
+    # convert to pixel diameter scale
+    sizes_pixel = np.round(np.sqrt(sizes_outercol/size_factor*2))
+    # create text labels
+    sizes_names = [str(int(s)) for s in sizes_outercol]
+    
+    # find dummy locations - put outside the map and hide with a white circle
+    lon_range = np.abs(geodata[loncol].max() - geodata[loncol].min())
+    idx = geodata[loncol].idxmax()   
+    dummy_lon = geodata[loncol][idx] + 0.1*lon_range
+    dummy_lat = geodata[latcol][idx]
+    
+    for ss in range(len(sizes_pixel)):
+        fig.add_trace(
+            go.Scattergeo(
+                lon=[dummy_lon],    # just dummy locations
+                lat=[dummy_lat],
+                name=sizes_names[ss],
+                # visible='legendonly',
+                marker=dict(
+                    size=sizes_pixel[ss],
+                    sizemode='area',
+                    color=line_colors['outer'],
+                    # opacity=0.5,
+                    line=dict(color=line_colors['marker'], width=1),
+                    ),
+                showlegend=True,
+                legendgroup=outercol,
+                hovertemplate=None, 
+                hoverinfo='skip', 
+                )
+            )
+    # plot a white marker over the top to hide them
+    fig.add_trace(
+        go.Scattergeo(
+            lon=[dummy_lon],
+            lat=[dummy_lat],
+            marker=dict(
+                size=legend_marker_pixel_max+3,
+                color='white',  
+                opacity=1,
+                ),
+            showlegend=False,
+            legendgroup='camouflage',
+            hovertemplate=None, 
+            hoverinfo='skip', 
+            ),
+        )
+        
+        
+    # Title
+    fig.update_layout(
+        legend_title_text=plotlabels['outerlabel'], 
+        legend_itemclick=False,
+        legend_itemdoubleclick=False,
+        )
+    
+    fig.update_layout(legend_itemsizing='trace')
+    
+    # change margins to smaller than default to get map to be bigger
+    fig.update_layout(margin=dict(l=30,b=20))
+    
+    # Only display this specific geography, not whole world
+    # fig.update_geos(fitbounds='locations', visible=False)
+    
+    # plot and save as html, with plotly JS library loaded from CDN
+    fig.write_html(
+        file=savefile,
+        default_height=fig_height,
+        include_plotlyjs='cdn',
+        )      
+    
+    # show the figure
+    if showfig:
+        os.startfile(savefile)
+    
+    return fig
+
 #%% Plot map
 # read shapefile of all USA counties
 countiesUSA = gpd.read_file('data\\geo\\cb_2019_us_county_500k.shp')
@@ -307,11 +504,25 @@ countiesWI = countiesUSA[countiesUSA.STATEFP == '55']
 prisons['Fraction infected'] = prisons['Positive Tests'] / prisons['Total population']
 prisons = prisons.sort_values('Total population', ascending=False)
 
-plotly_colorbubble2(
+# plotly_colorbubble2(
+#     countiesWI,
+#     prisons,
+#     colorcol='Fraction infected',
+#     outercol='Total population',
+#     loncol='Longitude',
+#     latcol='Latitude',
+#     popcol='Total population',
+#     color_range=[0,1],
+#     colorscale='BuPu',
+#     location_names=prisons.Name,
+#     )
+
+plotly_fillbubble(
     countiesWI,
     prisons,
-    colorcol='Fraction infected',
-    sizecol='Total population',
+    # colorcol='Fraction infected',
+    outercol='Total population',
+    innercol='Positive Tests',
     loncol='Longitude',
     latcol='Latitude',
     popcol='Total population',
