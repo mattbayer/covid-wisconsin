5be669eecc354c9f0be063a38451f12df0c56c7e
mattbayer <67168398+mattbayer@users.noreply.github.com>
2021-03-06 21:07:02 -0600

    Update BlogAgeAfterVaccines.py

--- BlogAgeAfterVaccines.py
+++ BlogAgeAfterVaccines.py
@@ -20,7 +20,7 @@ data_types = ['Cases', 'Hospitalizations', 'Deaths']
 
 # first 7-day boxcar average for weekly effects, 
 # then 5-day hamming to smooth more
-age_smooth = agedata.rolling(7).mean()
+age_smooth = agedata.rolling(14).mean()
 age_smooth = age_smooth.rolling(window=5, win_type='hamming').mean()
 
 # sum up larger age brackets
@@ -29,10 +29,12 @@ def larger_brackets(age_min):
         return '0-29'
     elif age_min < 60:
         return '30-59'
-    elif age_min < 80:
-        return '60-79'
+    # elif age_min < 80:
+    #     return '60-79'
+    # else:
+    #     return '80+'
     else:
-        return '80+'
+        return '60+'
     
 larger = age_smooth.melt(ignore_index=False)
 larger['Larger bracket'] = larger['Age bracket min'].apply(larger_brackets)
@@ -40,7 +42,7 @@ larger = larger.groupby(['Date', 'Data type', 'Larger bracket']).sum()
 larger = larger.drop(columns='Age bracket min')
 larger = larger.reset_index()
 # take out 0-29, it just confuses the plots
-larger = larger[larger['Larger bracket'] != '0-29']
+# larger = larger[larger['Larger bracket'] != '0-29']
 # re-pivot for plotting
 larger = larger.pivot(index='Date', columns=['Data type', 'Larger bracket'], values='value')
 
