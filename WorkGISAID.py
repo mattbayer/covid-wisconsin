af1992a5274b71fbde972486eaa9dc84ba9ab523
Matthew Bayer <67168398+mattbayer@users.noreply.github.com>
2021-04-10 22:39:12 -0500

    Variant / GISAID work

--- WorkGISAID.py
+++ WorkGISAID.py
@@ -17,8 +17,12 @@ from Bio import SeqIO
 
 #%% Load GISAID fasta file
 
-fasta_all = 'data\\sequences\\gisaid_hcov-19_wisconsin-all_2021_03_24_21.fasta'
-fasta_b117 = 'data\\sequences\\gisaid_hcov-19_wisconsin-b117_2021_03_24_21.fasta'
+# fasta_all = 'data\\sequences\\gisaid_hcov-19_wisconsin-all_2021_03_24_21.fasta'
+# fasta_b117 = 'data\\sequences\\gisaid_hcov-19_wisconsin-b117_2021_03_24_21.fasta'
+
+fasta_all = 'data\\sequences\\gisaid_hcov-19_wisconsin-all_2021_04_10_20.fasta'
+fasta_b117 = 'data\\sequences\\gisaid_hcov-19_wisconsin-b117_2021_04_10_20.fasta'
+
 
 # fasta_all = 'data\\sequences\\gisaid_hcov-19_michigan-all_2021_03_24_22.fasta'
 # fasta_b117 = 'data\\sequences\\gisaid_hcov-19_michigan-b117_2021_03_24_22.fasta'
@@ -45,11 +49,24 @@ def parse_fasta(fasta_file):
 gisaid_all = parse_fasta(fasta_all)
 gisaid_variants = parse_fasta(fasta_b117)
 
+gisaid_all.to_csv('data\\sequences\\gisaid-all-WI.csv', index=False)
+gisaid_variants.to_csv('data\\sequences\\gisaid-b117-WI.csv', index=False)
+
 #%% Manual file
 
 # gisaid_variants_file = 'data\\sequences\\gisaid_b117_manual_2021-03-11.csv'
 # gisaid_variants = pd.read_csv(gisaid_variants_file)
 # gisaid_variants['Collection date'] = pd.to_datetime(gisaid_variants['Collection date'])
+
+
+#%% TSV files
+
+path = 'data\\sequences'
+tsv_all = 'gisaid_wisconsin_all_metadata_2021-04-10.tsv'
+
+tsv_all = os.path.join(path, tsv_all)
+
+temp = pd.read_csv(tsv_all, sep='\t')
 
  
 
@@ -86,6 +103,7 @@ var_count['Variant fraction'] = var_count['Variants'] / var_count['Total']
 var_count = var_count.reset_index()
 
 var_count = var_count[var_count['Week of'] > datetime.datetime(2021, 1, 1)]
+var_count = var_count[var_count['Week of'] <= datetime.datetime(2021, 3, 15)]
 
 fig = px.line(var_count,
               x='Week of',
@@ -93,7 +111,7 @@ fig = px.line(var_count,
               # color_discrete_sequence='orange',
               )
 
-fig.update_layout(yaxis=dict(tickformat=".2%", range=[0, 0.2]), xaxis_range=[datetime.datetime(2021,1,17), datetime.datetime(2021, 3, 28)])
+fig.update_layout(yaxis=dict(tickformat=".2%", range=[0, 0.2]), xaxis_range=[datetime.datetime(2021,1,17), datetime.datetime(2021, 4, 5)])
 fig.update_traces(mode='lines+markers', line_color='orange', marker_color='orange', marker_symbol='cross')
 
 # save as html, with plotly JS library loaded from CDN
