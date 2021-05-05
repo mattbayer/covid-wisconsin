ebcf9645f3304a30d898f7fe88c521596d60f6c9
mattbayer <matthew.bayer@ge.com>
2021-05-05 10:11:15 -0500

    Update BlogVaccinesHalfway.py
    Add some initial plotting for by age and by race.

--- BlogVaccinesHalfway.py
+++ BlogVaccinesHalfway.py
@@ -15,16 +15,15 @@ import covid
 
 #%% Vaccination data
 
-# from Our World In Data github
+# from Our World In Data github, recording CDC data
 
-# vax_github = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv'
-# vax_df = pd.read_csv(vax_github)
+vax_github = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv'
+vax_df = pd.read_csv(vax_github, converters={'date': pd.to_datetime})
 
-vax_offline = 'data/vaccinations/us_state_vaccinations.csv'
-vax_df = pd.read_csv(vax_offline)
+# vax_offline = 'data/vaccinations/us_state_vaccinations.csv'
+# vax_df = pd.read_csv(vax_offline, converters={'date': pd.to_datetime})
 
 vax_wi = vax_df[vax_df.location == 'Wisconsin']
-vax_wi.date = pd.to_datetime(vax_wi.date.copy())
 
 daily = vax_wi.set_index('date').drop('location', axis=1).diff()
 avg7 = daily.rolling(7).mean()
@@ -35,3 +34,18 @@ vax_wi.plot(x='date', y=['total_distributed', 'total_vaccinations'])
 vax_wi.plot(x='date', y='share_doses_used')
 
 daily.plot(y=['total_distributed', 'total_vaccinations'])
+
+#%% Plot vax by age group from WI DHS data
+
+vax_age_file = 'data\\vaccinations\\Vax-Age-WI.csv'
+
+vax_age = pd.read_csv(vax_age_file, converters={'Reporting date': pd.to_datetime})
+
+vax_age.pivot(index='Reporting date', columns='Age group', values='Initiated %').plot()
+
+#%% Same for race
+vax_race_file = 'data\\vaccinations\\Vax-Race-WI.csv'
+
+vax_race = pd.read_csv(vax_race_file, converters={'Reporting date': pd.to_datetime})
+
+vax_race.pivot(index='Reporting date', columns='Race', values='Initiated %').plot()
