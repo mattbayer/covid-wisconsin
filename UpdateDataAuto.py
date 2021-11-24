faacb286761f57047ab3ce212ca912c9ef566c51
mattbayer <matthew.bayer@ge.com>
2021-11-24 10:01:20 -0600

    Wrap legacy plots in try/except

--- UpdateDataAuto.py
+++ UpdateDataAuto.py
@@ -10,41 +10,44 @@ Created on Thu Dec 24 12:33:03 2020
 import covid
 import datetime
 
-#%% Update data
-covid.update_covid_data_wi('state')
-covid.update_covid_data_wi('county')
-
-state = covid.read_covid_data_wi('state')
-
-
-#%% Dashboard - state line plots
-plotpath = '.\\docs\\_includes\\plotly'
-
-# reduce and rename at state level
-col_rename = {'POS_NEW': 'Cases', 'TEST_NEW': 'Tests', 'DTH_NEW': 'Deaths', 'HOSP_NEW': 'Hospitalizations'}
-state = state.rename(columns=col_rename)
-
-# Cases / Tests line plot
-covid.plotly_casetest(sourcedata=state, 
-                      case_col='Cases', 
-                      test_col='Tests', 
-                      date_col='Date', 
-                      savefile=plotpath + '\\Cases-Tests-WI.html',
-                      date_min=datetime.datetime(2021,1,15),
-                      range_max=2000,
-                      showfig=False,
-                      )
-
-# Deaths / Hospitalizations line plot
-covid.plotly_deadhosp(sourcedata=state, 
-                      hosp_col='Hospitalizations', 
-                      dead_col='Deaths', 
-                      date_col='Date', 
-                      savefile=plotpath + '\\Deaths-Hosp-WI.html',
-                      date_min=datetime.datetime(2021,1,15),
-                      range_max=100,
-                      showfig=False,
-                      )
+try:
+    #%% Update data
+    covid.update_covid_data_wi('state')
+    covid.update_covid_data_wi('county')
+    
+    state = covid.read_covid_data_wi('state')
+    
+    
+    #%% Dashboard - state line plots
+    plotpath = '.\\docs\\_includes\\plotly'
+    
+    # reduce and rename at state level
+    col_rename = {'POS_NEW': 'Cases', 'TEST_NEW': 'Tests', 'DTH_NEW': 'Deaths', 'HOSP_NEW': 'Hospitalizations'}
+    state = state.rename(columns=col_rename)
+    
+    # Cases / Tests line plot
+    covid.plotly_casetest(sourcedata=state, 
+                          case_col='Cases', 
+                          test_col='Tests', 
+                          date_col='Date', 
+                          savefile=plotpath + '\\Cases-Tests-WI.html',
+                          date_min=datetime.datetime(2021,1,15),
+                          range_max=2000,
+                          showfig=False,
+                          )
+    
+    # Deaths / Hospitalizations line plot
+    covid.plotly_deadhosp(sourcedata=state, 
+                          hosp_col='Hospitalizations', 
+                          dead_col='Deaths', 
+                          date_col='Date', 
+                          savefile=plotpath + '\\Deaths-Hosp-WI.html',
+                          date_min=datetime.datetime(2021,1,15),
+                          range_max=100,
+                          showfig=False,
+                          )
+except:
+    print("Update Legacy Data / Plots error")
 
 #%% Dashboard - monitoring plots
 
