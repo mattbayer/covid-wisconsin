aedb52bbabfe5b1d3a63adc3c96ea71d088fd63c
Matthew Bayer <67168398+mattbayer@users.noreply.github.com>
2022-02-14 21:05:39 -0600

    Vax eff over time work, plus elderly booster data

--- BlogVaccineEffectivenessOverTime.py
+++ BlogVaccineEffectivenessOverTime.py
@@ -30,6 +30,8 @@ urls = {'Cases': 'https://bi.wisconsin.gov/t/DHS/views/CasesOutcomesbyVaxStatus_
 
 #%%
 
+data_all = pd.DataFrame()
+
 for outcome in datasets:
     ts.loads(urls[outcome])
     dashboard = ts.getWorkbook()
@@ -45,8 +47,11 @@ for outcome in datasets:
     data = data.rename(columns=col_rename)
     data = data.drop('Fully Vax %', axis=1)
     data.columns.name = outcome + ' per 100k'
-    print(data)
-    
+    # print(data)
+
+    data = data.reset_index().melt(id_vars='Month', var_name='Vax status')
+    data['Outcome'] = outcome + ' per 100k'
+    data_all = data_all.append(data)
 
 #%% Load all the saved data
 
@@ -55,3 +60,40 @@ months = list(range(8,12))  # months 8:11 inclusive
 for m in months:
     filestr = 'Breakthroughs_2021-' + str(m).zfill(2) + '.csv'
     print(filestr)
+    
+    
+#%% Booster dose info
+# retrieved from https://covid.cdc.gov/covid-data-tracker/#vaccinations_vacc-people-additional-dose-count-pop65
+# on 14-Feb-2022, 8:36pm
+
+# total number of people in WI with a booster
+N_boost = 2_008_780
+
+# number over 65 with booster
+N_boost_65 = 757_863
+
+# percent fully vax w/ booster
+P_two_boost = 0.537
+
+# percent fully vax w/ booster, over 65
+P_two_boost_65 = 0.789
+
+
+# total number of people in WI two doses
+N_two = 3_738_069
+
+# number over 65 with booster
+N_two_65 = 960_823
+
+# percent with two doses
+P_two = 0.642
+
+# percent two doses, over 65
+P_two_65 = 0.945
+
+# or, just download the csv
+cdc_vax = pd.read_csv('data\\vaccinations\\cdc-vax-states_2022-02-14.csv')
+cdc_vax = cdc_vax.set_index('State/Territory/Federal Entity')
+
+P_boost_65 = (cdc_vax['Percent of 65+ Pop Fully Vaccinated by State of Residence']/100 
+              * cdc_vax['Percent of fully vaccinated people 65+ with booster doses']/100)
---
+++ data/vaccinations/cdc-vax-states-readme_2022-02-14.txt
@@ -0,0 +1,3 @@
+COVID-19 Vaccinations in the United States
+Date generated: Mon Feb 14 2022 20:53:21 GMT-0600 (Central Standard Time)
+'Other' includes doses of vaccine administered or delivered that are not from Pfizer-BioNTech Moderna or J&J.
