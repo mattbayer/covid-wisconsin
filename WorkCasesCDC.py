763b7464e9f4af8ee43fbda27751f53e5da5e93d
mattbayer <matthew.bayer@ge.com>
2022-02-23 16:09:36 -0600

    Direct download of CDC data

---
+++ WorkCasesCDC.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 15:36:02 2022
+
+@author: 212367548
+"""
+
+import pandas as pd
+import numpy as np
+import datetime
+import os
+
+import covid
+
+# Data from 
+# https://data.cdc.gov/Case-Surveillance/COVID-19-Case-Surveillance-Public-Use-Data/vbim-akqf
+
+cases_full = pd.read_csv('data\\United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
+
+#%%
+col_list = ['submission_date', 'state', 
+            'tot_cases', 'conf_cases', 'prob_cases',
+            'new_case', 'pnew_case',
+            ]
+            # 'created_at']
+
+cases = cases_full[col_list]
+cases.submission_date = pd.to_datetime(cases.submission_date)
+
+cases = cases.groupby('submission_date').sum()
+
+cases['new_case_7'] = cases.new_case.rolling(7).mean()
+
+cases = cases[cases.index >= pd.to_datetime('2021-12-31')]
+
+cases['projection'] = 1e5 * np.exp(-0.08*(cases.index.copy() - pd.to_datetime('2022-02-19')).days)
+
+cases.plot(y=['new_case_7', 'projection'], logy=True)
